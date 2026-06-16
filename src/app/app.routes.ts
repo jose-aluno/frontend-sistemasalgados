da0033b12bd5c3a99094c9c20867e87f422a1386
José Henrique Falsarella Lima Oliveira <josealuno70@gmail.com>
2026-06-15 22:41:17 -0300

    create initial components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Cadastro } from './pages/cadastro/cadastro';
+import { Cardapio } from './pages/cardapio/cardapio';
+import { authGuard } from './guards/auth-guard';
+import { Carrinho } from './pages/carrinho/carrinho';
+import { Historico } from './pages/historico/historico';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login  },
+  { path: 'cadastro', component: Cadastro},
+  { path: 'cardapio', component: Cardapio, canActivate: [authGuard] },
+  { path: 'carrinho', component: Carrinho, canActivate: [authGuard] },
+  { path: 'historico', component: Historico, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'login' }
+
+];
---
+++ src/app/pages/cardapio/cardapio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio',
+  imports: [],
+  templateUrl: './cardapio.html',
+  styleUrl: './cardapio.css',
+})
+export class Cardapio {
+
+}
